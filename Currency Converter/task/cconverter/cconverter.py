8b181ad19e5cb8267de154df8cd2c683295c03c3
Mohammad Al Bdour <mjalbdour@gmail.com>
2022-06-30 06:29:48 +0300

    Stage 3/6: More interaction

--- Currency Converter/task/cconverter/cconverter.py
+++ Currency Converter/task/cconverter/cconverter.py
@@ -1,9 +1,16 @@
 
-def conicoin_to_dollar(amount):
-    return amount * 100
+MSG_INPUT_CONICOINS = "Please, enter the number of conicoins you have:"
+MSG_INPUT_EXCHANGE_RATE = "Please, enter the exchange rate:"
+MSG_OUTPUT_DOLLARS = "The total amount of dollars:"
 
 
+def conicoin_to_dollar(amount, exchange_r):
+    return amount * exchange_r
+
+
+print(MSG_INPUT_CONICOINS)
 conicoins = int(input())
-print(f'I have {conicoins} conicoins.')
-print(f'{conicoins} conicoins cost {conicoin_to_dollar(conicoins)} dollars.')
-print('I am rich! Yippee!')
+print(MSG_INPUT_EXCHANGE_RATE)
+exchange_rate = float(input())
+result = conicoin_to_dollar(conicoins, exchange_rate)
+print(f'{MSG_OUTPUT_DOLLARS} {result}')
