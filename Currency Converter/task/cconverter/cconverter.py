c42a9814feeaf3988287b3cbfb65d2d26dca19b3
Mohammad Al Bdour <mjalbdour@gmail.com>
2022-06-30 06:37:46 +0300

    Stage 4/6: Going global

--- Currency Converter/task/cconverter/cconverter.py
+++ Currency Converter/task/cconverter/cconverter.py
@@ -2,15 +2,23 @@
 MSG_INPUT_CONICOINS = "Please, enter the number of conicoins you have:"
 MSG_INPUT_EXCHANGE_RATE = "Please, enter the exchange rate:"
 MSG_OUTPUT_DOLLARS = "The total amount of dollars:"
+MSG_OUTPUT_CURRENCY_1 = "I will get"
+MSG_OUT_CURRENCY_2 = "from the sale of"
+
+conicoin_to_currency = {
+    "RUB": 2.98,
+    "ARS": 0.82,
+    "HNL": 0.17,
+    "AUD": 1.9622,
+    "MAD": 0.208
+}
 
 
 def conicoin_to_dollar(amount, exchange_r):
-    return amount * exchange_r
+    return round(amount * exchange_r, 2)
 
 
-print(MSG_INPUT_CONICOINS)
-conicoins = int(input())
-print(MSG_INPUT_EXCHANGE_RATE)
-exchange_rate = float(input())
-result = conicoin_to_dollar(conicoins, exchange_rate)
-print(f'{MSG_OUTPUT_DOLLARS} {result}')
+conicoins = float(input())
+for currency, exchange_rate in conicoin_to_currency.items():
+    result = conicoin_to_dollar(conicoins, exchange_rate)
+    print(f'{MSG_OUTPUT_CURRENCY_1} {result} {currency} {MSG_OUT_CURRENCY_2} {conicoins} conicoins.')
