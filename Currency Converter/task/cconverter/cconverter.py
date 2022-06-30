5738fd2c0565565a2c3b9ccd36019d987f770df4
Mohammad Al Bdour <mjalbdour@gmail.com>
2022-06-30 06:13:40 +0300

    Stage 2/6: Talking numbers

--- Currency Converter/task/cconverter/cconverter.py
+++ Currency Converter/task/cconverter/cconverter.py
@@ -1,2 +1,9 @@
 
-print("Meet a conicoin!")
+def conicoin_to_dollar(amount):
+    return amount * 100
+
+
+conicoins = int(input())
+print(f'I have {conicoins} conicoins.')
+print(f'{conicoins} conicoins cost {conicoin_to_dollar(conicoins)} dollars.')
+print('I am rich! Yippee!')
